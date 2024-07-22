fd4101cca0ba84d500cb347515f3c1648a38bb56
Bick Renan <renanbickdev@gmail.com>
2024-07-22 16:23:27 -0300

    mudança em margin

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,7 @@
   background-size: cover;
   width: 100vw;
   height: 100vh;
-  margin: 0 25px 0 50px;
+  margin: 0 0 0 0;
 }
 
 
@@ -31,7 +31,7 @@
   top: 0;
   left: 0;
   width: 100%;
-  height: 110%;
+  height: 100%;
   background-color: rgba(0, 0, 0, 0.3);
 }
 
